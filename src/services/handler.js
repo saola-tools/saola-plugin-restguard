1ee6e0cf6c4036e230da73935ba47b3970d952b2
pnhung177 <pnhung177@acegik.net>
2019-08-24 23:04:23 +0700

    Move the verifyAccessToken() function out of Handler class

--- src/services/handler.js
+++ src/services/handler.js
@@ -58,7 +58,7 @@ function Handler(params = {}) {
   }
 
   this.verifyAccessToken = function (req, { promiseEnabled }) {
-    const result = verifyAccessToken(req);
+    const result = verifyAccessToken(req, sandboxConfig, errorBuilder, tracelogService, L, T);
     if (promiseEnabled) {
       if (result.error) {
         return Promise.reject(result.error);
@@ -66,72 +66,6 @@ function Handler(params = {}) {
       return Promise.resolve(result.token);
     } else {
       return result;
-    }
-  }
-
-  const verifyAccessToken = function (req) {
-    const requestId = tracelogService.getRequestId(req);
-    L.has('silly') && L.log('silly', T.add({ requestId }).toMessage({
-      tmpl: 'Req[${requestId}] - check header/url-params/post-body for access-token'
-    }));
-    let token = req.get(sandboxConfig.accessTokenHeaderName) ||
-        req.query[sandboxConfig.accessTokenParamsName] ||
-        req.params[sandboxConfig.accessTokenParamsName] ||
-        (req.body && req.body[sandboxConfig.accessTokenParamsName]);
-    if (token) {
-      L.has('debug') && L.log('debug', T.add({ requestId, token }).toMessage({
-        tmpl: 'Req[${requestId}] - access-token found: [${token}]'
-      }));
-      let tokenOpts = {
-        ignoreExpiration: sandboxConfig.ignoreExpiration || false
-      };
-      L.has('debug') && L.log('debug', T.add({ requestId, tokenOpts }).toMessage({
-        tmpl: 'Req[${requestId}] - Call jwt.verify() with options: ${tokenOpts}'
-      }));
-      try {
-        let tokenObject = jwt.verify(token, sandboxConfig.secretKey, tokenOpts);
-        L.has('debug') && L.log('debug', T.add({ requestId, tokenObject }).toMessage({
-          tmpl: 'Req[${requestId}] - Verification passed, token: ${tokenObject}'
-        }));
-        if (lodash.isFunction(sandboxConfig.accessTokenTransform)) {
-          tokenObject = sandboxConfig.accessTokenTransform(tokenObject);
-          L.has('debug') && L.log('debug', T.add({ requestId, tokenObject }).toMessage({
-            tmpl: 'Req[${requestId}] - transformed token: ${tokenObject}'
-          }));
-        }
-        req[sandboxConfig.accessTokenObjectName] = tokenObject;
-        return { token: tokenObject };
-      } catch (error) {
-        const language = extractLangCode(req);
-        L.has('debug') && L.log('debug', T.add({
-          requestId,
-          language,
-          error: { name: error.name, message: error.message }
-        }).toMessage({
-          tmpl: 'Req[${requestId}] - Verification failed, error: ${error}'
-        }));
-        if (error.name === 'TokenExpiredError') {
-          return {
-            error: errorBuilder.newError('TokenExpiredError', { language })
-          };
-        }
-        if (error.name === 'JsonWebTokenError') {
-          return {
-            error: errorBuilder.newError('JsonWebTokenError', { language })
-          };
-        }
-        return {
-          error: errorBuilder.newError('JwtVerifyUnknownError', { language })
-        };
-      }
-    } else {
-      const language = extractLangCode(req);
-      L.has('debug') && L.log('debug', T.add({ requestId }).toMessage({
-        tmpl: 'Req[${requestId}] - access-token not found'
-      }));
-      return {
-        error: errorBuilder.newError('TokenNotFoundError', { language })
-      };
     }
   }
 }
@@ -257,4 +191,70 @@ function processError (res, err) {
     body.payload = err.payload;
   }
   res.status(err.statusCode || 500).send(body);
+}
+
+const verifyAccessToken = function (req, sandboxConfig, errorBuilder, tracelogService, L, T) {
+  const requestId = tracelogService.getRequestId(req);
+  L.has('silly') && L.log('silly', T.add({ requestId }).toMessage({
+    tmpl: 'Req[${requestId}] - check header/url-params/post-body for access-token'
+  }));
+  let token = req.get(sandboxConfig.accessTokenHeaderName) ||
+      req.query[sandboxConfig.accessTokenParamsName] ||
+      req.params[sandboxConfig.accessTokenParamsName] ||
+      (req.body && req.body[sandboxConfig.accessTokenParamsName]);
+  if (token) {
+    L.has('debug') && L.log('debug', T.add({ requestId, token }).toMessage({
+      tmpl: 'Req[${requestId}] - access-token found: [${token}]'
+    }));
+    let tokenOpts = {
+      ignoreExpiration: sandboxConfig.ignoreExpiration || false
+    };
+    L.has('debug') && L.log('debug', T.add({ requestId, tokenOpts }).toMessage({
+      tmpl: 'Req[${requestId}] - Call jwt.verify() with options: ${tokenOpts}'
+    }));
+    try {
+      let tokenObject = jwt.verify(token, sandboxConfig.secretKey, tokenOpts);
+      L.has('debug') && L.log('debug', T.add({ requestId, tokenObject }).toMessage({
+        tmpl: 'Req[${requestId}] - Verification passed, token: ${tokenObject}'
+      }));
+      if (lodash.isFunction(sandboxConfig.accessTokenTransform)) {
+        tokenObject = sandboxConfig.accessTokenTransform(tokenObject);
+        L.has('debug') && L.log('debug', T.add({ requestId, tokenObject }).toMessage({
+          tmpl: 'Req[${requestId}] - transformed token: ${tokenObject}'
+        }));
+      }
+      req[sandboxConfig.accessTokenObjectName] = tokenObject;
+      return { token: tokenObject };
+    } catch (error) {
+      const language = extractLangCode(req);
+      L.has('debug') && L.log('debug', T.add({
+        requestId,
+        language,
+        error: { name: error.name, message: error.message }
+      }).toMessage({
+        tmpl: 'Req[${requestId}] - Verification failed, error: ${error}'
+      }));
+      if (error.name === 'TokenExpiredError') {
+        return {
+          error: errorBuilder.newError('TokenExpiredError', { language })
+        };
+      }
+      if (error.name === 'JsonWebTokenError') {
+        return {
+          error: errorBuilder.newError('JsonWebTokenError', { language })
+        };
+      }
+      return {
+        error: errorBuilder.newError('JwtVerifyUnknownError', { language })
+      };
+    }
+  } else {
+    const language = extractLangCode(req);
+    L.has('debug') && L.log('debug', T.add({ requestId }).toMessage({
+      tmpl: 'Req[${requestId}] - access-token not found'
+    }));
+    return {
+      error: errorBuilder.newError('TokenNotFoundError', { language })
+    };
+  }
 }
