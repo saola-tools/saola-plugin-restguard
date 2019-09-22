aabab4c6e035e0fb3368cbe375ef0a6f19fd41d1
pnhung177 <pnhung177@acegik.net>
2019-09-23 05:15:26 +0700

    Provide the helper to the world

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-module.exports = require('devebot').registerLayerware(__dirname, [
+'use strict';
+
+var plugin = require('devebot').registerLayerware(__dirname, [
   'app-errorlist',
   'app-tracelog',
   'app-webweaver'
 ], []);
+
+var moduleMapping = {
+  helper: './lib/utils/chores.js'
+}
+
+plugin.require = function (moduleName) {
+  if (moduleName in moduleMapping) {
+    return require(moduleMapping[moduleName]);
+  }
+  return null;
+}
+
+module.exports = plugin;
--- src/utils/chores.js
+++ src/utils/chores.js
@@ -4,6 +4,17 @@ const Devebot = require('devebot');
 const lodash = Devebot.require('lodash');
 
 function Chores() {
+  this.stringToArray = function (labels) {
+    labels = labels || '';
+    if (typeof labels === 'string') {
+      return labels.split(',').map(function(item) {
+        return item.trim();
+      }).filter(function(item) {
+        return item.length > 0;
+      });
+    }
+    return labels;
+  };
   this.renameJsonFields = function (data, nameMappings) {
     if (nameMappings && lodash.isObject(nameMappings)) {
       for (const oldName in nameMappings) {
