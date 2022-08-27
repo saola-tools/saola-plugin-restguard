6c29475d7eea14fd6a88839f6ae1f34c05e9c85c
pnhung177 <pnhung177@gmail.com>
2022-08-27 14:43:24 +0700

    Enhance the tokenkit service

--- src/services/tokenkit.js
+++ src/services/tokenkit.js
@@ -1,33 +1,47 @@
 'use strict';
 
 const lodash = require('lodash');
-const jwt = require('jsonwebtoken');
 const moment = require('moment');
+const jwt = require('jsonwebtoken');
+
+const jwt_sign_options_names = [
+  'algorithms', 'expiresIn', 'notBefore', 'audience', 'issuer', 'jwtid',
+  'subject', 'noTimestamp', 'header', 'keyid', 'mutatePayload'
+]
+
+const jwt_verify_options_names = [
+  'algorithms', 'audience', 'complete', 'issuer', 'ignoreExpiration', 'ignoreNotBefore',
+  'subject', 'clockTolerance', 'maxAge', 'clockTimestamp', 'nonce'
+]
 
 function Service (params = {}) {
   const { sandboxConfig, loggingFactory } = params;
   const L = loggingFactory.getLogger();
   const T = loggingFactory.getTracer();
 
-  const config = lodash.pick(sandboxConfig, ['expiredIn', 'secretKey']);
+  const config = lodash.pick(sandboxConfig, ['secretKey', 'expiredIn']);
   config.secretKey = config.secretKey || 't0ps3cr3t'
   config.expiredIn = config.expiredIn || 60 * 60; // expires in 1 hour
 
-  this.encode = function(data, more) {
+  this.encode = function(data, opts) {
+    opts = opts || {};
+    //
     const now = moment();
-    const expiredIn = config.expiredIn;
-    const expiredTime = now.add(expiredIn, "seconds").toDate();
+    const secretKey = opts.secretKey || config.secretKey;
+    const expiresIn = opts.expiresIn || config.expiredIn;
+    const expiredTime = now.add(expiresIn, "seconds").toDate();
     //
     const accessObject = Object.assign({}, data, {
-      expiredIn,
+      expiredIn: expiresIn,
       expiredTime
     });
     //
     const auth = {
-      access_token: jwt.sign(accessObject, config.secretKey, {
-        expiresIn: data.expiredIn || config.expiredIn
+      access_token: jwt.sign(accessObject, secretKey, {
+        expiresIn: expiresIn,
+        ...lodash.pick(opts, jwt_sign_options_names)
       }),
-      expires_in: expiredIn,
+      expires_in: expiresIn,
       expired_time: expiredTime
     };
     //
@@ -35,7 +49,14 @@ function Service (params = {}) {
   }
 
   this.decode = function(token, opts) {
-    return jwt.verify(token, config.secretKey, opts);
+    opts = opts || {};
+    //
+    const secretKey = opts.secretKey || config.secretKey;
+    const ignoreExpiration = config.ignoreExpiration || undefined;
+    return jwt.verify(token, secretKey, {
+      ignoreExpiration,
+      ...lodash.pick(opts, jwt_verify_options_names)
+    });
   }
 }
 
