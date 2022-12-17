179f373578ace9f91d089705a61fcf0ec5c3f0ee
pnhung177 <pnhung177@gmail.com>
2022-12-18 05:19:18 +0700

    Resolve some issues of eslint checking

--- test/tdd/handler-test.js
+++ test/tdd/handler-test.js
@@ -1,38 +1,38 @@
-'use strict';
+"use strict";
 
-const devebot = require('devebot');
-const Promise = devebot.require('bluebird');
-const lodash = devebot.require('lodash');
-const { jsonwebtoken: jwt } = require('tokenlib');
-const path = require('path');
-const assert = require('chai').assert;
-const dtk = require('liberica').mockit;
+const devebot = require("devebot");
+const Promise = devebot.require("bluebird");
+const lodash = devebot.require("lodash");
+const { jsonwebtoken: jwt } = require("tokenlib");
+const path = require("path");
+const assert = require("chai").assert;
+const dtk = require("liberica").mockit;
 
-describe('handler', function() {
+describe("handler", function() {
   const loggingFactory = dtk.createLoggingFactoryMock({ captureMethodCall: false });
   const ctx = {
     L: loggingFactory.getLogger(),
     T: loggingFactory.getTracer(),
-    blockRef: 'app-restguard/handler',
-  }
+    blockRef: "app-restguard/handler",
+  };
 
-  describe('extractBypassingRules()', function() {
+  describe("extractBypassingRules()", function() {
     let Handler, extractBypassingRules;
 
     beforeEach(function() {
-      Handler = dtk.acquire('handler', { libraryDir: '../lib' });
-      extractBypassingRules = dtk.get(Handler, 'extractBypassingRules');
+      Handler = dtk.acquire("handler", { libraryDir: "../lib" });
+      extractBypassingRules = dtk.get(Handler, "extractBypassingRules");
     });
 
-    it('should transform the bypassingRules configuration properly', function () {
+    it("should transform the bypassingRules configuration properly", function () {
       assert.deepEqual(extractBypassingRules({}), {});
       assert.deepEqual(extractBypassingRules({bypassingRules: 1024 }), {});
       assert.deepEqual(extractBypassingRules({bypassingRules: { abc: 1, def: 2 } }), {});
       assert.deepEqual(extractBypassingRules({
         bypassingRules: {
           inclusion: {
-            hostnames: 'example.*',
-            ips: '127.0.0.1'
+            hostnames: "example.*",
+            ips: "127.0.0.1"
           },
           def: 2
         }
@@ -49,8 +49,8 @@ describe('handler', function() {
             hostnames: "example\\.(com|net|org)"
           },
           exclusion: {
-            hostnames: ['example.com'],
-            ips: ['127.0.0.1']
+            hostnames: ["example.com"],
+            ips: ["127.0.0.1"]
           }
         }
       }), {
@@ -59,131 +59,131 @@ describe('handler', function() {
           ips: null
         },
         exclusion: {
-          hostnames: ['example.com'],
-          ips: ['127.0.0.1']
+          hostnames: ["example.com"],
+          ips: ["127.0.0.1"]
         }
       });
-    })
+    });
   });
 
-  describe('isBypassed()', function() {
+  describe("isBypassed()", function() {
     let Handler, isBypassed;
 
     beforeEach(function() {
-      Handler = dtk.acquire('handler', { libraryDir: '../lib' });
-      isBypassed = dtk.get(Handler, 'isBypassed');
+      Handler = dtk.acquire("handler", { libraryDir: "../lib" });
+      isBypassed = dtk.get(Handler, "isBypassed");
     });
 
-    it('should skip bypassing if the enabled is false', function () {
+    it("should skip bypassing if the enabled is false", function () {
       const bypassingRules = {
         enabled: false,
         exclusion: {
-          hostnames: ['example.com'],
-          ips: ['192.168.1.1']
+          hostnames: ["example.com"],
+          ips: ["192.168.1.1"]
         }
       };
-      assert.isFalse(isBypassed({ hostname: 'example.com', ip: '127.0.0.1' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'example.com', ip: '192.168.1.1' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'devebot.com', ip: '192.168.1.1' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'devebot.com', ip: '127.0.0.1' }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "example.com", ip: "127.0.0.1" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "example.com", ip: "192.168.1.1" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "devebot.com", ip: "192.168.1.1" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "devebot.com", ip: "127.0.0.1" }, bypassingRules));
     });
 
-    it('should satisfy the exclusion bypassing rules (array)', function () {
+    it("should satisfy the exclusion bypassing rules (array)", function () {
       const bypassingRules = {
         exclusion: {
-          hostnames: ['example.com'],
-          ips: ['192.168.1.101', '192.168.1.102']
+          hostnames: ["example.com"],
+          ips: ["192.168.1.101", "192.168.1.102"]
         }
       };
-      assert.isFalse(isBypassed({ hostname: 'example.com', ip: '127.0.0.1' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'example.com', ip: '192.168.1.102' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'devebot.com', ip: '192.168.1.102' }, bypassingRules));
-      assert.isTrue(isBypassed({ hostname: 'devebot.com', ip: '127.0.0.1' }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "example.com", ip: "127.0.0.1" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "example.com", ip: "192.168.1.102" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "devebot.com", ip: "192.168.1.102" }, bypassingRules));
+      assert.isTrue(isBypassed({ hostname: "devebot.com", ip: "127.0.0.1" }, bypassingRules));
     });
 
-    it('should satisfy the exclusion bypassing rules (regexp)', function () {
+    it("should satisfy the exclusion bypassing rules (regexp)", function () {
       const bypassingRules = {
         exclusion: {
           hostnames: /example\.(com|net|org)/,
-          ips: ['192.168.1.101', '192.168.1.102']
+          ips: ["192.168.1.101", "192.168.1.102"]
         }
       };
-      assert.isFalse(isBypassed({ hostname: 'example.com', ip: '127.0.0.1' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'www.example.org', ip: '127.0.0.1' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'example.net', ip: '192.168.1.102' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'devebot.com', ip: '192.168.1.102' }, bypassingRules));
-      assert.isTrue(isBypassed({ hostname: 'devebot.com', ip: '127.0.0.1' }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "example.com", ip: "127.0.0.1" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "www.example.org", ip: "127.0.0.1" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "example.net", ip: "192.168.1.102" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "devebot.com", ip: "192.168.1.102" }, bypassingRules));
+      assert.isTrue(isBypassed({ hostname: "devebot.com", ip: "127.0.0.1" }, bypassingRules));
     });
 
-    it('should satisfy the inclusion bypassing rules (array)', function () {
+    it("should satisfy the inclusion bypassing rules (array)", function () {
       const bypassingRules = {
         inclusion: {
-          hostnames: ['example.com'],
-          ips: ['192.168.1.101', '192.168.1.102']
+          hostnames: ["example.com"],
+          ips: ["192.168.1.101", "192.168.1.102"]
         }
       };
-      assert.isTrue(isBypassed({ hostname: 'example.com', ip: '127.0.0.1' }, bypassingRules));
-      assert.isTrue(isBypassed({ hostname: 'example.com', ip: '192.168.1.102' }, bypassingRules));
-      assert.isTrue(isBypassed({ hostname: 'devebot.com', ip: '192.168.1.102' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'devebot.com', ip: '127.0.0.1' }, bypassingRules));
+      assert.isTrue(isBypassed({ hostname: "example.com", ip: "127.0.0.1" }, bypassingRules));
+      assert.isTrue(isBypassed({ hostname: "example.com", ip: "192.168.1.102" }, bypassingRules));
+      assert.isTrue(isBypassed({ hostname: "devebot.com", ip: "192.168.1.102" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "devebot.com", ip: "127.0.0.1" }, bypassingRules));
     });
 
-    it('should satisfy the inclusion bypassing rules (regexp)', function () {
+    it("should satisfy the inclusion bypassing rules (regexp)", function () {
       const bypassingRules = {
         inclusion: {
           hostnames: /example\.(com|net|org)/,
-          ips: ['192.168.1.101', '192.168.1.102']
+          ips: ["192.168.1.101", "192.168.1.102"]
         }
       };
-      assert.isTrue(isBypassed({ hostname: 'example.com', ip: '127.0.0.1' }, bypassingRules));
-      assert.isTrue(isBypassed({ hostname: 'www.example.org', ip: '127.0.0.1' }, bypassingRules));
-      assert.isTrue(isBypassed({ hostname: 'example.net', ip: '192.168.1.102' }, bypassingRules));
-      assert.isTrue(isBypassed({ hostname: 'devebot.com', ip: '192.168.1.102' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'devebot.com', ip: '127.0.0.1' }, bypassingRules));
+      assert.isTrue(isBypassed({ hostname: "example.com", ip: "127.0.0.1" }, bypassingRules));
+      assert.isTrue(isBypassed({ hostname: "www.example.org", ip: "127.0.0.1" }, bypassingRules));
+      assert.isTrue(isBypassed({ hostname: "example.net", ip: "192.168.1.102" }, bypassingRules));
+      assert.isTrue(isBypassed({ hostname: "devebot.com", ip: "192.168.1.102" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "devebot.com", ip: "127.0.0.1" }, bypassingRules));
     });
 
-    it('should satisfy the both of inclusion and exclusion bypassing rules', function () {
+    it("should satisfy the both of inclusion and exclusion bypassing rules", function () {
       const bypassingRules = {
         exclusion: {
-          hostnames: ['example.com', 'testing.com'],
-          ips: ['192.168.1.101', '192.168.1.102']
+          hostnames: ["example.com", "testing.com"],
+          ips: ["192.168.1.101", "192.168.1.102"]
         },
         inclusion: {
           hostnames: /example\.(com|net|org)/,
-          ips: ['192.168.1.102', '192.168.1.103']
+          ips: ["192.168.1.102", "192.168.1.103"]
         }
       };
-      assert.isFalse(isBypassed({ hostname: 'example.com', ip: '127.0.0.1' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'unknown.com', ip: '192.168.1.102' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'booking.net', ip: '127.0.0.1' }, bypassingRules));
-      assert.isFalse(isBypassed({ hostname: 'devebot.com', ip: '192.168.1.101' }, bypassingRules));
-      assert.isTrue(isBypassed({ hostname: 'devebot.com', ip: '192.168.1.103' }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "example.com", ip: "127.0.0.1" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "unknown.com", ip: "192.168.1.102" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "booking.net", ip: "127.0.0.1" }, bypassingRules));
+      assert.isFalse(isBypassed({ hostname: "devebot.com", ip: "192.168.1.101" }, bypassingRules));
+      assert.isTrue(isBypassed({ hostname: "devebot.com", ip: "192.168.1.103" }, bypassingRules));
     });
   });
 
-  const app = require(path.join(__dirname, '../../app'));
-  const sandboxConfig = lodash.get(app.config, ['sandbox', 'default', 'plugins', 'appRestguard']);
+  const app = require(path.join(__dirname, "../../app"));
+  const sandboxConfig = lodash.get(app.config, ["sandbox", "default", "plugins", "appRestguard"]);
   false && console.log(JSON.stringify(sandboxConfig, null, 2));
 
-  const tracelogService = app.runner.getSandboxService('app-tracelog/tracelogService');
-  const errorManager = app.runner.getSandboxService('app-errorlist/manager');
-  const errorBuilder = errorManager.register('app-restguard', sandboxConfig);
+  const tracelogService = app.runner.getSandboxService("app-tracelog/tracelogService");
+  const errorManager = app.runner.getSandboxService("app-errorlist/manager");
+  const errorBuilder = errorManager.register("app-restguard", sandboxConfig);
   const secretKeys = [ sandboxConfig.secretKey ];
 
-  describe('verifyAccessToken()', function() {
+  describe("verifyAccessToken()", function() {
     let Handler, verifyAccessToken;
     const serviceContext = lodash.assign({ sandboxConfig, secretKeys, errorBuilder, tracelogService }, ctx);
 
     beforeEach(function() {
-      Handler = dtk.acquire('handler', { libraryDir: '../lib' });
-      verifyAccessToken = dtk.get(Handler, 'verifyAccessToken');
+      Handler = dtk.acquire("handler", { libraryDir: "../lib" });
+      verifyAccessToken = dtk.get(Handler, "verifyAccessToken");
     });
 
-    it('return ok when a valid accessToken provided', function () {
-      const data = { message: 'example' };
+    it("return ok when a valid accessToken provided", function () {
+      const data = { message: "example" };
       const req = new ExpressRequestMock({
         headers: {
-          'X-Access-Token': createAccessToken(data, sandboxConfig.secretKey, 60)
+          "X-Access-Token": createAccessToken(data, sandboxConfig.secretKey, 60)
         }
       });
       const result = verifyAccessToken(req, serviceContext);
@@ -192,17 +192,17 @@ describe('handler', function() {
       assert.isUndefined(result.error);
     });
 
-    it('support using multiple secretKeys to validate a accessToken (passed)', function () {
-      const data = { message: 'example' };
+    it("support using multiple secretKeys to validate a accessToken (passed)", function () {
+      const data = { message: "example" };
       const req = new ExpressRequestMock({
         headers: {
-          'X-Access-Token': createAccessToken(data, sandboxConfig.secretKey, 60)
+          "X-Access-Token": createAccessToken(data, sandboxConfig.secretKey, 60)
         }
       });
       const serviceContextCopied = lodash.cloneDeep(serviceContext);
       serviceContextCopied.secretKeys = [
-        'unknown',
-        'invalid',
+        "unknown",
+        "invalid",
         serviceContextCopied.sandboxConfig.secretKey
       ];
       const result = verifyAccessToken(req, serviceContextCopied);
@@ -211,42 +211,42 @@ describe('handler', function() {
       assert.isUndefined(result.error);
     });
 
-    it('support using multiple secretKeys to validate a accessToken (failed)', function () {
-      const data = { message: 'example' };
+    it("support using multiple secretKeys to validate a accessToken (failed)", function () {
+      const data = { message: "example" };
       const req = new ExpressRequestMock({
         headers: {
-          'X-Access-Token': createAccessToken(data, sandboxConfig.secretKey, 60)
+          "X-Access-Token": createAccessToken(data, sandboxConfig.secretKey, 60)
         }
       });
       const serviceContextCopied = lodash.cloneDeep(serviceContext);
       serviceContextCopied.secretKeys = [
-        'invalid',
-        'unmatched',
-        'unknown'
+        "invalid",
+        "unmatched",
+        "unknown"
       ];
       const result = verifyAccessToken(req, serviceContextCopied);
       assert.isUndefined(result.token);
       assert.isObject(result.error);
-      assert.equal(result.error.name, 'JsonWebTokenError');
+      assert.equal(result.error.name, "JsonWebTokenError");
     });
 
-    it('throw a JsonWebTokenError if an unmatched secretKey provided', function () {
+    it("throw a JsonWebTokenError if an unmatched secretKey provided", function () {
       const req = new ExpressRequestMock({
         headers: {
-          'X-Access-Token': createAccessToken({}, sandboxConfig.secretKey + '-another', 60)
+          "X-Access-Token": createAccessToken({}, sandboxConfig.secretKey + "-another", 60)
         }
       });
       const result = verifyAccessToken(req, serviceContext);
       assert.isUndefined(result.token);
       assert.isObject(result.error);
-      assert.equal(result.error.name, 'JsonWebTokenError');
+      assert.equal(result.error.name, "JsonWebTokenError");
     });
 
-    it('throw a TokenExpiredError if the access-token has expired', function () {
-      const data = { message: 'example' };
+    it("throw a TokenExpiredError if the access-token has expired", function () {
+      const data = { message: "example" };
       const req = new ExpressRequestMock({
         headers: {
-          'X-Access-Token': createAccessToken(data, sandboxConfig.secretKey, 1) // 1 second
+          "X-Access-Token": createAccessToken(data, sandboxConfig.secretKey, 1) // 1 second
         }
       });
       return Promise.resolve().delay(1100).then(function() {
@@ -257,28 +257,28 @@ describe('handler', function() {
         const result = verifyAccessToken(req, serviceContextCopied);
         assert.isUndefined(result.token);
         assert.isObject(result.error);
-        assert.equal(result.error.name, 'TokenExpiredError');
-      })
+        assert.equal(result.error.name, "TokenExpiredError");
+      });
     });
 
-    it('throw a TokenNotFoundError if a token could not be found in header, query or body', function () {
+    it("throw a TokenNotFoundError if a token could not be found in header, query or body", function () {
       const req = new ExpressRequestMock();
       const result = verifyAccessToken(req, serviceContext);
       assert.isUndefined(result.token);
       assert.isObject(result.error);
-      assert.equal(result.error.name, 'TokenNotFoundError');
+      assert.equal(result.error.name, "TokenNotFoundError");
     });
   });
 });
 
 function createAccessToken (data = {}, secretKey, expiresIn) {
-  const token = jwt.sign(data, secretKey || 't0ps3cr3t', {
+  const token = jwt.sign(data, secretKey || "t0ps3cr3t", {
     expiresIn: expiresIn || 60 * 1000
   });
   return token;
 }
 
-function ExpressRequestMock(kwargs = {}) {
+function ExpressRequestMock (kwargs = {}) {
   const store = { };
 
   store.headers = lodash.mapKeys(kwargs.headers, function(value, key) {
@@ -286,18 +286,18 @@ function ExpressRequestMock(kwargs = {}) {
   });
 
   const self = this;
-  lodash.forEach (['query', 'params'], function(fieldName) {
+  lodash.forEach (["query", "params"], function(fieldName) {
     self[fieldName] = {};
     if (fieldName in kwargs) {
       if (kwargs[fieldName] && lodash.isObject(kwargs[fieldName])) {
         self[fieldName] = lodash.cloneDeep(kwargs[fieldName]);
       } else {
-        throw Error('ExpressRequestMock[' + fieldName + '] must be an object');
+        throw Error("ExpressRequestMock[" + fieldName + "] must be an object");
       }
     }
   });
 
   this.get = function(name) {
     return store.headers[lodash.lowerCase(name)];
-  }
+  };
 }
--- test/tdd/toolkit-test.js
+++ test/tdd/toolkit-test.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
-const assert = require('chai').assert;
-const mockit = require('liberica').mockit;
-const timekeeper = require('timekeeper');
+const assert = require("chai").assert;
+const mockit = require("liberica").mockit;
+const timekeeper = require("timekeeper");
 
-describe('toolkit', function() {
+describe("toolkit", function() {
   const loggingFactory = mockit.createLoggingFactoryMock({ captureMethodCall: false });
 
-  describe('toolkit.verify()', function() {
+  describe("toolkit.verify()", function() {
     let Toolkit, toolkit;
 
     const sandboxConfig = {
       expiresIn: 60,
       ignoreExpiration: false,
       secretKey: "dobietday",
-    }
+    };
 
     const tc = {
       "current": "2020-07-17T11:22:33.123Z",
@@ -23,16 +23,16 @@ describe('toolkit', function() {
       "data": {
         "message": "example"
       },
-      "token": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
-          + '.eyJtZXNzYWdlIjoiZXhhbXBsZSIsImlhdCI6MTU5NDk4NDk1MywiZXhwIjoxNTk0OTg1MDEzfQ'
-          + '.lB8BCCbIyF1WB9qRvvNR4UuNTMCWInWe9Yr-ArqoMmw',
+      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" +
+          ".eyJtZXNzYWdlIjoiZXhhbXBsZSIsImlhdCI6MTU5NDk4NDk1MywiZXhwIjoxNTk0OTg1MDEzfQ" +
+          ".lB8BCCbIyF1WB9qRvvNR4UuNTMCWInWe9Yr-ArqoMmw",
       "iat": 1594984953,
       "exp": 1594985013
-    }
+    };
 
-    describe('toolkit.verify() - signature', function() {
+    describe("toolkit.verify() - signature", function() {
       beforeEach(function() {
-        Toolkit = mockit.acquire('toolkit', { libraryDir: '../lib' });
+        Toolkit = mockit.acquire("toolkit", { libraryDir: "../lib" });
         timekeeper.freeze(new Date(tc.current));
       });
 
@@ -40,15 +40,15 @@ describe('toolkit', function() {
         timekeeper.reset();
       });
 
-      it('return ok when a valid jwt token provided', function () {
+      it("return ok when a valid jwt token provided", function () {
         toolkit = new Toolkit({ sandboxConfig, loggingFactory });
         const result = toolkit.verify(tc.token);
         assert.isObject(result);
         assert.deepInclude(result, tc.data);
       });
 
-      it('use the secretKey from the options first (passed)', function () {
-        toolkit = new Toolkit({ 
+      it("use the secretKey from the options first (passed)", function () {
+        toolkit = new Toolkit({
           sandboxConfig: Object.assign({}, sandboxConfig, { secretKey: "invalid" }),
           loggingFactory
         });
@@ -57,17 +57,17 @@ describe('toolkit', function() {
         assert.deepInclude(result, tc.data);
       });
 
-      it('use the secretKey from the options first (failed)', function () {
+      it("use the secretKey from the options first (failed)", function () {
         toolkit = new Toolkit({ sandboxConfig, loggingFactory });
         assert.throws(function() {
           const result = toolkit.verify(tc.token, { secretKey: "invalid" });
-        }, Error, "invalid signature")
+        }, Error, "invalid signature");
       });
     });
 
-    describe('toolkit.verify() - expiration', function() {
+    describe("toolkit.verify() - expiration", function() {
       beforeEach(function() {
-        Toolkit = mockit.acquire('toolkit', { libraryDir: '../lib' });
+        Toolkit = mockit.acquire("toolkit", { libraryDir: "../lib" });
         timekeeper.freeze(new Date(tc.expiredTime_plus_2));
       });
 
@@ -75,22 +75,22 @@ describe('toolkit', function() {
         timekeeper.reset();
       });
 
-      it('expiration error occured', function () {
+      it("expiration error occured", function () {
         toolkit = new Toolkit({ sandboxConfig, loggingFactory });
         assert.throws(function() {
           const result = toolkit.verify(tc.token);
-        }, Error, "jwt expired")
+        }, Error, "jwt expired");
       });
 
-      it('ignoreExpiration affected (from options)', function () {
+      it("ignoreExpiration affected (from options)", function () {
         toolkit = new Toolkit({ sandboxConfig, loggingFactory });
         const result = toolkit.verify(tc.token, { ignoreExpiration: true });
         assert.isObject(result);
         assert.deepInclude(result, tc.data);
       });
 
-      it('ignoreExpiration affected (from config)', function () {
-        toolkit = new Toolkit({ 
+      it("ignoreExpiration affected (from config)", function () {
+        toolkit = new Toolkit({
           sandboxConfig: Object.assign({}, sandboxConfig, { ignoreExpiration: true }),
           loggingFactory
         });
